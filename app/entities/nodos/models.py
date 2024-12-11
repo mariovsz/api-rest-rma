0381ad2c0631fc995f429ee4b5c5055505f3d1d8
mariovsz <vsqzma@gmail.com>
2024-12-11 14:31:54 -0300

    nodos

---
+++ app/entities/nodos/models.py
@@ -0,0 +1,23 @@
+from typing import List, Optional
+
+from sqlalchemy import Float, Integer, String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from ..basemodel import ModeloBase
+
+
+class Nodo(ModeloBase):
+    __tablename__ = "nodos"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    identificador: Mapped[str] = mapped_column(String, unique=True, index=True)
+    descripcion: Mapped[str] = mapped_column(String, index=True)
+    porcentajeBateria: Mapped[int] = mapped_column(Integer, index=True)
+    latitud: Mapped[float] = mapped_column(Float, index=True, nullable=True)
+    longitud: Mapped[float] = mapped_column(Float, index=True, nullable=True)
+    is_active: Mapped[bool] = mapped_column(
+        Boolean, index=True, nullable=True, default=True
+    )
+
+    # Relación con Paquete
+    paquetes: Mapped[List["Paquete"]] = relationship("Paquete", back_populates="nodo")
