f815f2720f29c5b0fb3cb64cf2b2795fbdc5110b
mariovsz <vsqzma@gmail.com>
2024-12-11 14:35:16 -0300

    config logging

---
+++ app/config/logging_config.py
@@ -0,0 +1,46 @@
+import logging
+from logging.config import dictConfig
+
+# Tu configuración de logging personalizada
+custom_log_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": "%(levelprefix)s %(message)s",
+            "use_colors": None,
+        },
+        "access": {
+            "()": "uvicorn.logging.AccessFormatter",
+            "fmt": '%(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s',
+        },
+        "detailed": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - [%(pathname)s:%(lineno)d] %(message)s",
+        },
+    },
+    "handlers": {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+        "access": {
+            "formatter": "access",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "loggers": {
+        "uvicorn": {"handlers": ["default"], "level": "INFO", "propagate": False},
+        "uvicorn.error": {"level": "INFO", "handlers": ["default"], "propagate": True},
+        "uvicorn.access": {"handlers": ["access"], "level": "INFO", "propagate": False},
+        "mi_aplicacion": {"handlers": ["default"], "level": "DEBUG", "propagate": True},
+    },
+}
+
+
+def setup_logging():
+    dictConfig(custom_log_config)
+    logger = logging.getLogger("mi_aplicacion")
+    return logger
