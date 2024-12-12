d1e5816298b7c6b4cfc1e83b724fa19eda71c472
mariovsz <vsqzma@gmail.com>
2024-12-12 18:42:54 -0300

    config

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from ..database.database import engine
-from ..database.basemodel import ModeloBase
-from ..database.paquetes.router import router as paquete_router
-from ..MQTT.iot_thread import IoTThread
+from .database.basemodel import ModeloBase
+from .database.database import engine
+from .database.paquetes.router import router as paquete_router
+from .database.nodos.router import router as nodos_router
+from .MQTT.iot_thread import IoTThread
 
 
 # Función callback para procesar mensajes MQTT (opcional)
@@ -19,8 +20,6 @@ def process_mqtt_message(message: str):
 load_dotenv()
 ENV = os.getenv("ENV", "DEV")
 ROOT_PATH = os.getenv(f"ROOT_PATH_{ENV.upper()}", "")
-
-# Variables de MQTT
 MQTT_TOPIC = os.getenv("MQTT_TOPIC", "")
 MQTT_BROKER = os.getenv("MQTT_BROKER", "")
 MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
@@ -30,7 +29,7 @@ MQTT_KEEPALIVE = int(os.getenv("MQTT_KEEPALIVE", 60))
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     ModeloBase.metadata.create_all(bind=engine)
-    # Iniciar el hilo IoT cuando se inicie la aplicación
+    # Iniciar el hilo MQTT
     iot_thread = IoTThread(
         MQTT_BROKER,
         MQTT_PORT,
@@ -41,19 +40,18 @@ async def lifespan(app: FastAPI):
     iot_thread.start()
 
     yield
-    # Detener el hilo IoT cuando se detenga la aplicación
+    # Detener el hilo MQTT
     iot_thread.stop()
     iot_thread.join()
 
 
 # Crear la aplicación FastAPI
 app = FastAPI(root_path=ROOT_PATH, lifespan=lifespan)
-
-
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
+
 
 # Configuración de CORS
 app.add_middleware(
@@ -70,4 +68,9 @@ app.include_router(
     paquete_router,
     prefix="/paquetes",
     tags=["Paquetes"],
+)
+app.include_router(
+    nodos_router,
+    prefix="/nodos",
+    tags=["Nodos"],
 )
